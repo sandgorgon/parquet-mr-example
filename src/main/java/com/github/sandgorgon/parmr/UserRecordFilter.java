2bce6fd238dd4c9da22232a7caacecfe5d8262a3
sandgorgon <ramondevera@gmail.com>
2015-07-27 16:26:19 -0600

    Update UserRecordFilter.java

--- src/main/java/com/github/sandgorgon/parmr/UserRecordFilter.java
+++ src/main/java/com/github/sandgorgon/parmr/UserRecordFilter.java
@@ -1,11 +1,21 @@
 /*
- * Copyright (C)2015 Micron Technology Inc. All Rights Reserved.
- * Unpublished - all rights reserved under the copyright laws of the  United States.
- * USE OF A COPYRIGHT NOTICE IS PRECAUTIONARY ONLY AND  DOES NOT IMPLY PUBLICATION OR DISCLOSURE.
- * 
- * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF MICRON TECHNOLOGY, INC. USE, DISCLOSURE,
- * OR REPRODUCTION IS PROHIBITED WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF MICRON TECHNOLOGY, INC.
- */
+Copyright (c) 2015. Ramon de Vera Jr.
+All Rights Reserved
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to use
+, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of 
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 package com.github.sandgorgon.parmr;
 
 import parquet.column.ColumnReader;
